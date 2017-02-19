19e5880fe8200f3c3ea243706da48686c61c2b80
geeny21 <geeny21@daum.net>
2017-02-20 05:46:12 +0900

    sprintf() dtostrf() 함수를 이용한 sprintf 및 직렬통신을 이용한 확인

---
+++ test_sprintf.ino
@@ -0,0 +1,21 @@
+void setup() {
+  // put your setup code here, to run once:
+  char str[60];
+  int i = 3;
+  int j = 2;
+  float f;
+  Serial.begin(9600);
+  Serial.println("start");
+  sprintf(str,"%3d+%3d=%3d\n",i,j,i+j);
+  Serial.println(str);
+
+  f = float(i)/float(j);
+  dtostrf(f, 5,2, str);
+  Serial.println(str);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
